14b254c242e55470e63f584c86a4ebba56cf1724
Abigail González E <pamelagon12@gmail.com>
2022-06-29 20:40:32 -0600

    Logica, service minado

---
+++ apiBlockChain/apiBlockChain/LogicB/LogicMinado.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiBlockChain.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace apiBlockChain.LogicB
+{
+    public class LogicMinado
+    {
+        public BloqueB bloque;
+        public LogicMinado(BloqueB _bloque)
+        {
+         
+          
+        }
+
+        public LogicMinado()
+        {
+        }
+
+        public string GenerateHash256(string text) {
+
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(text));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+
+        public DateTime generateFechaHora() {
+            DateTime localDate = DateTime.Now;
+            localDate.ToString("en-GB");
+            string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+            
+            return localDate;
+        }
+
+        public long generateDateToInt(DateTime fechaMinado) {
+            DateTime centuryBegin = new DateTime(2001, 1, 1);
+            return (fechaMinado.Ticks - centuryBegin.Ticks);
+           
+        }
+
+        public string generateHash(string sha256) {
+
+            GenerateHash256(sha256);
+
+            return "";
+        }
+
+  
+
+    }
+
+
+}
+
+
---
+++ apiBlockChain/apiBlockChain/Models/BloqueB.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiBlockChain.Models
+{
+    public class BloqueB
+    {
+       
+        public BloqueB(int _idBloque,string _fechaMinado,int _prueba,int _milisegundos) {
+
+            idBloque = _idBloque;
+            fechaMinado = _fechaMinado;
+            prueba = _prueba;
+            milisegundos = _milisegundos;
+
+        }
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("idBloque")]
+        public int idBloque { get; set; }
+
+        [BsonElement("fechaMinado")]
+        public string fechaMinado { get; set; }
+
+        [BsonElement("prueba")]
+        public int prueba { get; set; }
+
+        [BsonElement("milisegundos")]
+        public int milisegundos { get; set; }
+
+        [BsonElement("documentos")]
+        public string [] documentos { get; set; }
+
+        [BsonElement("hashPrevio")]
+        public string hashPrevio { get; set; }
+
+        [BsonElement("hash")]
+        public string hash { get; set; }
+
+
+    }
+}
